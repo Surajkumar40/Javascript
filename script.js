ab726a17d487654a25c6d299d266b985533cd0e7
Surajkumar40 <surajk74643@gmail.com>
2025-01-04 18:29:08 -0800

    Add files via upload
    In this we learn about the variable in the javascript

---
+++ script.js
@@ -0,0 +1,22 @@
+// Variables in js
+// Variable are the container that holds the data in it 
+
+
+const accountName = "Suraj Kumar"
+// const is a type of variable in which the variable will not change 
+let accountPassword = "12345"
+// let is a type of variable in which the variable is exists in and value can be changed whithin the block the block scope and function scope and it is widely used in programs
+var city = "jaipur"
+// the var is a type of variable in which the variable is globally declared and we use it in all over program and its value can be changed  it is less use in present time due to its value change. 
+gmailId = "Suraj@gmail.com"
+// we can declare a variable without using hte keyword let, var and const in which it is 
+let accountState;// we can define varibles without assinging the value it it output is undefined
+
+// accountName="suraj" is is not change it through error because the constant variable not change its value
+accountPassword="111111"
+city="Mumbai"
+gmailId="kumar123.com"
+
+console.log(accountName);// it is used to print the text
+console.log(accountState); 
+console.table([accountName, accountPassword ,city, gmailId]);// it is used to print the variables and its vallue in tabular form.
